8b3207feaba1a81feca1108f0466693970328058
mallasaugat <saugat.smalla@gmail.com>
2024-08-29 22:28:55 +0000

    Changes

--- change-data.py
+++ change-data.py
@@ -17,17 +17,14 @@ with open("datasets/md-data.csv", newline='') as csvfile:
             newList = list(row)
             newStatement = ""
 
-            newStatement += (
-            f"Gender: {'Male' if newList[2] == 'M' else 'Female'}, "
-            f"Age: {newList[5]}, "
-            f"Appointment Date: {newList[3]}, "
-            f"Reason: {newList[4]}, "
-            f"Hypertension: {'YES' if int(newList[8]) else 'NO'}, "
-            f"Diabetes: {'YES' if int(newList[9]) else 'NO'}, "
-            f"Alcoholism: {'YES' if int(newList[10]) else 'NO'}, "
-            f"Handicap: {'YES' if int(newList[11]) else 'NO'}, "
-            f"SMS Received: {'YES' if int(newList[12]) else 'NO'}."
-        )
+            newStatement = (
+                f"The patient is a {'male' if newList[2] == 'M' else 'female'}, aged {newList[5]}. "
+                f"They have an appointment scheduled on {newList[3]} for the reason of {newList[4]}. "
+                f"The patient has a history of hypertension: {'yes' if int(newList[8]) else 'no'}, "
+                f"diabetes: {'yes' if int(newList[9]) else 'no'}, "
+                f"alcoholism: {'yes' if int(newList[10]) else 'no'}, and a handicap: {'yes' if int(newList[11]) else 'no'}. "
+                f"Additionally, they have received an SMS confirmation: {'yes' if int(newList[12]) else 'no'}."
+            )
 
             
             writer.writerow({'Statement': newStatement, 'NoShow': newList[-1] })
